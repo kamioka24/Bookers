b94ad5d25b9174d47269c512ab3f85893043cefd
上岡圭介 <kamioka_24@kyapunoMacBook-Pro.local>
2020-04-24 17:31:43 +0900

    show,edit,top終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,39 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+  	@book = Book.new
+  	@books = Book.all
   end
 
   def show
-  end
-
-  def new
+  	@book = Book.find(params[:id])
   end
 
   def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to "/books"
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def update
+  	@book = Book.find(params[:id])
+    @book.update(book_params)
   end
 
   def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to "/books"
   end
-  
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
 end
